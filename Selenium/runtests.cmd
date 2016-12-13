d6b690fa42d08460ab41d7911801b596c1365a78
Xiaolong <m15701007635@163.com>
2016-12-13 18:14:22 +0800

    add sendemail

--- Selenium/runtests.cmd
+++ Selenium/runtests.cmd
@@ -7,10 +7,17 @@
 @cd ..\packages\SpecRun.Runner.*\tools
 
 @set profile=%1
+@set repostFile = %date:~,4%%date:~5,2%%date:~8,2%
 @if "%profile%" == "" set profile=Default
 
-SpecRun.exe run %profile%.srprofile "/baseFolder:%~dp0\bin\Debug" /log:specrun.log %2 %3 %4 %5
+SpecRun.exe run %profile%.srprofile "/baseFolder:%~dp0\bin\Debug" /log:specrun.log /report:%~dp0\TestResult\%repostFile%.html
 
 :end
  
+cd d:
+cd Source\eclipse\workspace\interfaceDemo\interface
+python smtp_op.py --receiver xiaolong.chen@dilatoit.com --content "AutoTestReport" --attachments %~dp0\TestResult\%repostFile%.html
+
 @popd
+
+
